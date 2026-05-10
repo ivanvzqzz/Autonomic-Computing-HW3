0a0122595aed79940aa962c2154f94c83326c346
Iván Vázquez <vazzp.ctd@gmail.com>
2026-05-09 22:15:05 -0700

    Add MAPE-K knowledge base

--- mape-k/mape_k/dataset.py
+++ mape-k/mape_k/dataset.py
@@ -3,8 +3,8 @@ from pathlib import Path
 from loguru import logger
 from tqdm import tqdm
 import typer
-
-from mape_k.config import PROCESSED_DATA_DIR, RAW_DATA_DIR
+import pandas as pd
+from mape_k.config import RAW_DATA_DIR, INTERIM_DATA_DIR
 
 app = typer.Typer()
 
@@ -12,15 +12,17 @@ app = typer.Typer()
 @app.command()
 def main(
     # ---- REPLACE DEFAULT PATHS AS APPROPRIATE ----
-    input_path: Path = RAW_DATA_DIR / "dataset.csv",
-    output_path: Path = PROCESSED_DATA_DIR / "dataset.csv",
+    input_path: Path = RAW_DATA_DIR / "sensor1.csv",
+    output_path: Path = INTERIM_DATA_DIR / "dataset.csv",
     # ----------------------------------------------
 ):
-    # ---- REPLACE THIS WITH YOUR OWN CODE ----
+    # ---- SETTING INDEX AND COLUMNS ----
     logger.info("Processing dataset...")
-    for i in tqdm(range(10), total=10):
-        if i == 5:
-            logger.info("Something happened for iteration 5.")
+    data_df = pd.read_csv(input_path)
+    cols_to_use = ["timestamp", "vibration_x", "vibration_y", "vibration_z", "class"]
+    data_df = data_df[cols_to_use]
+    data_df = data_df.set_index("timestamp")
+    data_df.to_csv(output_path)
     logger.success("Processing dataset complete.")
     # -----------------------------------------
 
